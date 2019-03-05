e0745faf03100a5ee0119ac001f3e1f6c39639b4
Eiichiro Watanabe <a16tochjp@gmail.com>
2019-03-05 15:01:09 +0900

    Refactor tests in resource

--- resource_test.go
+++ resource_test.go
@@ -2,122 +2,172 @@ package main
 
 import (
 	"io/ioutil"
-	"log"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-type exampleResource struct {
-	Name    string
-	Content string
-}
-
-var cases = []*exampleResource{
-	{
-		"case1",
-		"route: 192.168.1.0/24\norigin: AS65001\nsource: TEST\n\nroute: 192.168.2.0/24\norigin: AS65002\nremarks: maxLength 26\nsource: TEST\n\nroute6: 2001:db8:1::/48\norigin: AS65001\nsource: TEST\n\nroute6: 2001:db8:2::/48\norigin: AS65002\nremarks: maxLength 64\nsource: TEST\n\n",
-	},
-}
-
-var expected = []struct {
-	route            string
-	ip               string
-	prefixLen        int
-	maxLengthEnable  int
-	maxLengthDisable int
-	origin           int
+var examples = map[string]struct {
+	Content []string
+  UseMaxLen bool
+	ExpectedRoute            string
+	ExpectedIp               string
+	ExpectedPrefixLen        int
+	ExpectedMaxLen  int
+	ExpectedOriginAs          int
 }{
-	{
+	"IPv4_WithUseMaxLen_WithoutRemarks": {
+    []string{
+	    "route: 192.168.1.0/24\n",
+      "origin: AS65001\n",
+      "source: TEST\n",
+      "\n",
+    },
+    true,
 		"192.168.1.0/24",
 		"192.168.1.0",
 		24,
 		32,
-		24,
 		65001,
 	},
-	{
-		"192.168.2.0/24",
-		"192.168.2.0",
-		24,
-		26,
-		26,
-		65002,
+	"IPv4_WithUseMaxLen_WithRemarks": {
+    []string{
+      "route: 192.168.2.0/24\n",
+      "origin: AS65002\n",
+      "remarks: maxLength 26\n",
+      "source: TEST\n",
+      "\n",
+    },
+    true,
+	  "192.168.2.0/24",
+	  "192.168.2.0",
+	  24,
+	  26,
+	  65002,
+  },
+	"IPv6_WithUseMaxLen_WithoutRemarks": {
+    []string{
+      "route6: 2001:db8:1::/48\n",
+      "origin: AS65001\n",
+      "source: TEST\n",
+      "\n",
+    },
+    true,
+	  "2001:db8:1::/48",
+	  "2001:db8:1::",
+	  48,
+	  128,
+	  65001,
 	},
-	{
-		"2001:db8:1::/48",
-		"2001:db8:1::",
-		48,
-		128,
-		48,
-		65001,
-	},
-	{
-		"2001:db8:2::/48",
-		"2001:db8:2::",
-		48,
-		64,
-		64,
-		65002,
-	},
+	"IPv6_WithUseMaxLen_WithRemarks": {
+    []string{
+      "route6: 2001:db8:2::/48\n",
+      "origin: AS65002\n",
+      "remarks: maxLength 64\n",
+      "source: TEST\n",
+      "\n",
+    },
+    true,
+	  "2001:db8:2::/48",
+	  "2001:db8:2::",
+	  48,
+	  64,
+	  65002,
+  },
+	"IPv4_WithoutUseMaxLen_WithoutRemarks": {
+    []string{
+	    "route: 192.168.1.0/24\n",
+      "origin: AS65001\n",
+      "source: TEST\n",
+      "\n",
+    },
+    false,
+	  "192.168.1.0/24",
+	  "192.168.1.0",
+	  24,
+	  24,
+	  65001,
+  },
+	"IPv4_WithoutUseMaxLen_WithRemarks": {
+    []string{
+      "route: 192.168.2.0/24\n",
+      "origin: AS65002\n",
+      "remarks: maxLength 26\n",
+      "source: TEST\n",
+      "\n",
+    },
+    false,
+	  "192.168.2.0/24",
+	  "192.168.2.0",
+	  24,
+	  26,
+	  65002,
+  },
+	"IPv6_WithoutUseMaxLen_WithoutRemarks": {
+    []string{
+      "route6: 2001:db8:1::/48\n",
+      "origin: AS65001\n",
+      "source: TEST\n",
+      "\n",
+    },
+    false,
+	  "2001:db8:1::/48",
+	  "2001:db8:1::",
+	  48,
+	  48,
+	  65001,
+  },
+	"IPv6_WithoutUseMaxLen_WithRemarks": {
+    []string{
+      "route6: 2001:db8:2::/48\n",
+      "origin: AS65002\n",
+      "remarks: maxLength 64\n",
+      "source: TEST\n",
+      "\n",
+    },
+    false,
+	  "2001:db8:2::/48",
+	  "2001:db8:2::",
+	  48,
+	  64,
+	  65002,
+  },
 }
 
-func generateFiles() []string {
-	files := []string{}
-	for _, c := range cases {
-		tmpfile, _ := ioutil.TempFile(os.TempDir(), c.Name)
-		if _, err := tmpfile.Write([]byte(c.Content)); err != nil {
-			log.Fatal(err)
-		}
-		files = append(files, tmpfile.Name())
-		tmpfile.Close()
+func createFile(name string, content []string) string {
+	tmpfile, _ := ioutil.TempFile(os.TempDir(), name)
+  for _, str := range content {
+	  tmpfile.WriteString(str)
 	}
-	return files
+	tmpfile.Close()
+	return tmpfile.Name()
 }
 
-func removeFiles(files []string) {
-	for _, f := range files {
-		os.Remove(f)
-	}
+func removeFile(fileName string) {
+	os.Remove(fileName)
 }
 
-func TestResourceWithUseMaxlen(t *testing.T) {
-	assert := assert.New(t)
-	files := generateFiles()
-	r, _ := newResource(files, true)
-	for _, ex := range expected {
-		rf, n, maxLen, _ := parseCIDR(ex.route)
-		_ = maxLen
-		addr := n.IP
-		m, _ := n.Mask.Size()
-		plen := uint8(m)
-		b, _ := r.table[r.currentSN][rf].Get(generateKey(rf, addr, plen))
-		bucket := b.(*prefixResource)
-		assert.Equal(ex.ip, bucket.prefix.String())
-		assert.Equal(ex.prefixLen, int(bucket.prefixLen))
-		assert.Equal(ex.maxLengthEnable, int(bucket.values[0].maxLen))
-		assert.Equal(ex.origin, int(bucket.values[0].asns[0]))
-	}
-	removeFiles(files)
-}
+func TestResource(t *testing.T) {
+	for name, v := range examples {
+	  tmpFile := createFile(name, v.Content)
+    t.Run(name, func(t *testing.T) {
+	    assert := assert.New(t)
+	    r, _ := newResource([]string{tmpFile}, v.UseMaxLen)
 
-func TestResourceWithoutUseMaxlen(t *testing.T) {
-	assert := assert.New(t)
-	files := generateFiles()
-	r, _ := newResource(files, false)
-	for _, ex := range expected {
-		rf, n, maxLen, _ := parseCIDR(ex.route)
-		_ = maxLen
-		addr := n.IP
-		m, _ := n.Mask.Size()
-		plen := uint8(m)
-		b, _ := r.table[r.currentSN][rf].Get(generateKey(rf, addr, plen))
-		bucket := b.(*prefixResource)
-		assert.Equal(ex.ip, bucket.prefix.String())
-		assert.Equal(ex.prefixLen, int(bucket.prefixLen))
-		assert.Equal(ex.maxLengthDisable, int(bucket.values[0].maxLen))
-		assert.Equal(ex.origin, int(bucket.values[0].asns[0]))
+		  rf, n, maxLen, _ := parseCIDR(v.ExpectedRoute)
+		  _ = maxLen
+		  addr := n.IP
+		  m, _ := n.Mask.Size()
+		  plen := uint8(m)
+		  b, _ := r.table[r.currentSN][rf].Get(generateKey(rf, addr, plen))
+		  bucket := b.(*prefixResource)
+
+		  assert.Equal(v.ExpectedIp, bucket.prefix.String())
+		  assert.Equal(v.ExpectedPrefixLen, int(bucket.prefixLen))
+		  assert.Equal(v.ExpectedMaxLen, int(bucket.values[0].maxLen))
+		  assert.Equal(v.ExpectedOriginAs, int(bucket.values[0].asns[0]))
+    })
+	  os.Remove(tmpFile)
 	}
-	removeFiles(files)
 }
